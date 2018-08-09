0eb6ec7a91de76969a7d3b4cf0d4e46ddf52e514
Alex Franco <aadf82@gmail.com>
2018-08-09 13:11:31 -0400

    Added support for browserSync. Added source paths for Sass.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reloac
 
 var sassPaths = [
   'node_modules/foundation-sites/scss',
@@ -16,9 +18,20 @@ gulp.task('sass', function() {
     .pipe($.autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9']
     }))
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('default', ['sass'], function() {
-  gulp.watch(['scss/**/*.scss'], ['sass']);
+
+  browserSync.init({
+    notify: false,
+    proxy: 'localhost:8888/fasttrac-static',
+    port: 8080
+  });
+
+  //gulp.watch(['scss/**/*.scss'], ['sass']);
+  gulp.watch("./scss/*.scss", ['sass']);
+  gulp.watch("./sass/*.sass", ['sass']);
+  gulp.watch("./*.php").on('change', browserSync.reload);
 });

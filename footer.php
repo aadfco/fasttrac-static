570bd590210f370b7bf57b097c80911edddfecef
Alex Franco <aadf82@gmail.com>
2018-08-14 18:02:37 -0400

    Added site-wrapper

--- footer.php
+++ footer.php
@@ -1,62 +1,63 @@
-<footer>
-  <nav class="main-footer">
-    <div class="container">
-      <div class="inner-row">
-        <div class="footer-widget-cell">
-          <h5 class="standard-shadow">fast trac rewards</h5>
-          <ul class="footer-widget-links">
-            <li><a href="">redeem points</a></li>
-            <li><a href="">earn points</a></li>
-            <li><a href="">log in</a></li>
-            <li><a href="">sign up</a></li>
-          </ul>
+  <footer>
+    <nav class="main-footer">
+      <div class="container">
+        <div class="inner-row">
+          <div class="footer-widget-cell">
+            <h5 class="standard-shadow">fast points</h5>
+            <ul class="footer-widget-links">
+              <li><a href="">redeem points</a></li>
+              <li><a href="">earn points</a></li>
+              <li><a href="">log in</a></li>
+              <li><a href="">sign up</a></li>
+            </ul>
+          </div>
+          <div class="footer-widget-cell">
+            <h5 class="standard-shadow">locations</h5>
+            <ul class="footer-widget-links">
+              <li><a href="">location search</a></li>
+              <li><a href="">fuel safety</a></li>
+              <li><a href="">food</a></li>
+              <li><a href="">beverages</a></li>
+            </ul>
+          </div>
+          <div class="footer-widget-cell">
+            <h5 class="standard-shadow">about</h5>
+            <ul class="footer-widget-links">
+              <li><a href="">about fast Trac</a></li>
+              <li><a href="">FAQ</a></li>
+              <li><a href="">News</a></li>
+              <li><a href="">contact us</a></li>
+            </ul>
+          </div>
+          <div class="footer-widget-cell">
+            <h5 class="standard-shadow">legal</h5>
+            <ul class="footer-widget-links">
+              <li><a href="">privacy policy</a></li>
+              <li><a href="">terms &amp; conditions</a></li>
+              <li><a href="">site terms of use</a></li>
+            </ul>
+          </div>
         </div>
-        <div class="footer-widget-cell">
-          <h5 class="standard-shadow">locations</h5>
-          <ul class="footer-widget-links">
-            <li><a href="">location search</a></li>
-            <li><a href="">fuel safety</a></li>
-            <li><a href="">food</a></li>
-            <li><a href="">beverages</a></li>
-          </ul>
-        </div>
-        <div class="footer-widget-cell">
-          <h5 class="standard-shadow">about</h5>
-          <ul class="footer-widget-links">
-            <li><a href="">about fast Trac</a></li>
-            <li><a href="">FAQ</a></li>
-            <li><a href="">News</a></li>
-            <li><a href="">contact us</a></li>
-          </ul>
-        </div>
-        <div class="footer-widget-cell">
-          <h5 class="standard-shadow">legal</h5>
-          <ul class="footer-widget-links">
-            <li><a href="">privacy policy</a></li>
-            <li><a href="">terms &amp; conditions</a></li>
-            <li><a href="">site terms of use</a></li>
-          </ul>
+      </div>
+    </nav>
+    <div class="bottom-footer">
+      <div class="container">
+        <div class="inner-row bottom-footer-align">
+          <div class="bf-copyright">
+            <p>&copy; <?php echo date("Y"); ?> Fast Trac. All rights reserved.</p>
+          </div>
+          <div class="bf-social">
+            <ul class="menu">
+              <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+              <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+              <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
-  </nav>
-  <div class="bottom-footer">
-    <div class="container">
-      <div class="inner-row bottom-footer-align">
-        <div class="bf-copyright">
-          <p>&copy; <?php echo date("Y"); ?> Fast Trac. All rights reserved.</p>
-        </div>
-        <div class="bf-social">
-          <ul class="menu">
-            <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>
-</footer>
+  </footer>
+</div><!-- end site-wrapper -->
 
 <script src="js/jquery.js"></script>
 <script src="js/what-input.js"></script>

aa182ef9eb2eb57460dc8aeba5aa5664749f14a9
Alex Franco <aadf82@gmail.com>
2018-08-14 18:02:59 -0400

    Added site-wrapper. Use new hamburger menu.

--- header.php
+++ header.php
@@ -9,84 +9,84 @@
     <link rel="stylesheet" href="css/app.css">
   </head>
   <body>
-    <header class="navigation">
-      <div class="top-nav">
-        <div class="container">
-          <div class="inner-row align-justify">
-            <div class="top-social">
-              <ul class="menu">
-                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-              </ul>
-            </div>
-            <nav class="secondary-menu">
-              <ul class="menu">
-                <li><a href="#">about us</a></li>
-                <li><a href="#">news</a></li>
-                <li><a href="#">community</a></li>
-                <li><a href="#">careers</a></li>
-                <li><a href="#">contact</a></li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </div><!-- end top-nav -->
-
-      <div class="main-nav">
-        <div class="container">
-          <div class="inner-row align-justify align-middle">
-            <div class="logo">
-              <a href="/fasttrac-static"><img src="img/fasttrac-logo-web.svg" alt="Fast Trac Convenience Store"></a>
-            </div>
-
-            <nav class="mobile-menu hide-for-large">
-              <div class="mobile-btn-wrap">
-                <div class="mobile-btn" data-toggle="mobile-menu-overlay">
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                </div>
-              </div>
-              <div class="mobile-menu-overlay" id="mobile-menu-overlay" data-toggler=".mobile-menu-open" data-animate="slide-in-down slide-out-up">
-                <ul class="primary-mobile-menu">
-                  <li><a href="#">fast points</a></li>
-                  <li><a href="#">fresh food</a></li>
-                  <li><a href="#">coffee &amp; drinks</a></li>
-                  <li><a href="#">locations</a></li>
+    <div class="site-wrapper">
+      <header class="navigation">
+        <div class="top-nav">
+          <div class="container">
+            <div class="inner-row">
+              <div class="top-social">
+                <ul class="menu">
+                  <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                  <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                  <li><a href="#"><i class="fab fa-twitter"></i></a></li>
                 </ul>
-                <ul class="secondary-mobile-menu">
+              </div>
+              <nav class="secondary-menu">
+                <ul class="menu">
                   <li><a href="#">about us</a></li>
                   <li><a href="#">news</a></li>
                   <li><a href="#">community</a></li>
                   <li><a href="#">careers</a></li>
                   <li><a href="#">contact</a></li>
                 </ul>
-              </div>
-            </nav>
-
-            <nav class="primary-menu">
-              <ul class="menu">
-                <li><a href="#">fast points</a></li>
-                <li><a href="#">fresh food</a></li>
-                <li><a href="#">coffee &amp; drinks</a></li>
-                <li><a href="#">locations</a></li>
-              </ul>
-            </nav>
-
-            <div class="fp-login">
-              <div class="vertical-divider"></div>
-              <div class="fp-logo">
-                <a href="#">
-                  <img src="img/fastpoints-v2.svg" alt="Fast Trac Fast Points Login | Sign up">
-                  <span class="fp-login-link">Login | Sign Up</span>
-                </a>
-              </div>
+              </nav>
             </div>
           </div>
-        </div>
-      </div> <!-- end main-nav -->
-    </header> <!-- end navigation -->
+        </div><!-- end top-nav -->
+
+        <!-- Begin Main Nav -->
+        <div class="main-nav">
+          <div class="container">
+            <div class="inner-row align-justify align-middle">
+              <div class="logo">
+                <a href="/fasttrac-static"><img src="img/fasttrac-logo-web.svg" alt="Fast Trac Convenience Store"></a>
+              </div>
+
+              <nav class="mobile-menu hide-for-large">
+                <div class="mobile-btn-wrap">
+                  <button class="hamburger hamburger--elastic" type="button" data-toggle="mobile-menu-overlay">
+                    <span class="hamburger-box">
+                      <span class="hamburger-inner"></span>
+                    </span>
+                  </button>
+                </div>
+                <div class="mobile-menu-overlay" id="mobile-menu-overlay" data-toggler=".mobile-menu-open" data-animate="hinge-in-from-top hinge-out-from-top">
+                  <ul class="primary-mobile-menu">
+                    <li><a href="#">fast points</a></li>
+                    <li><a href="#">fresh food</a></li>
+                    <li><a href="#">coffee &amp; drinks</a></li>
+                    <li><a href="#">locations</a></li>
+                  </ul>
+                  <ul class="secondary-mobile-menu">
+                    <li><a href="#">about us</a></li>
+                    <li><a href="#">news</a></li>
+                    <li><a href="#">community</a></li>
+                    <li><a href="#">careers</a></li>
+                    <li><a href="#">contact</a></li>
+                  </ul>
+                </div>
+              </nav>
+
+              <!-- Begin Desktop Navigation Menus -->
+              <nav class="primary-menu">
+                <ul class="menu">
+                  <li><a href="#">fast points</a></li>
+                  <li><a href="#">fresh food</a></li>
+                  <li><a href="#">coffee &amp; drinks</a></li>
+                  <li><a href="#">locations</a></li>
+                </ul>
+              </nav>
+
+              <div class="fp-login">
+                <div class="fp-logo">
+                  <a href="#">
+                    <img src="img/fastpoints-v2.svg" alt="Fast Trac Fast Points Login | Sign up">
+                    <span class="fp-login-link">Login | Sign Up</span>
+                  </a>
+                </div>
+              </div>
+
+            </div> <!-- end inner-row -->
+          </div> <!-- end container -->
+        </div> <!-- end main-nav -->
+      </header> <!-- end navigation -->

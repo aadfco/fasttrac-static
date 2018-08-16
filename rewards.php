cf9e68904cffe4eca4946e5dcf3943b4df7c01f8
Alex Franco <aadf82@gmail.com>
2018-08-16 03:59:40 -0400

    Intial commit

---
+++ rewards.php
@@ -0,0 +1,126 @@
+<?php include('header.php'); ?>
+
+  <div class="marketing-site-hero">
+    <div class="marketing-site-hero-content">
+      <h1 class="standard-shadow">earn rewards immediately at checkout!</h1>
+      <p class="subheader">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam omnis, maxime libero natus qui minus!</p>
+      <a href="#" class="button">learn more</a>
+    </div>
+  </div>
+
+  <div class="how-to-earn">
+    <div class="container">
+      <div class="inner-row">
+        <div class="section-title">
+          <h1 class="superhero">how do i earn points?</h1>
+        </div>
+      </div>
+
+      <div class="card-wrapper">
+        <div class="grid-x grid-padding-x small-up-1 medium-up-3">
+        <div class="cell">
+          <div class="card">
+            <div class="card-icon-circle">
+              <div class="card-icon">
+                <i class="fas fa-star"></i>
+              </div>
+            </div>
+            <div class="card-section">
+              <h4>Earn Points</h4>
+              <p>Simply make a qualifying purchase at Fast Trac and immediately receive discounts at checkout.</p>
+            </div>
+          </div>
+        </div>
+        <div class="cell">
+          <div class="card">
+            <div class="card-icon-circle">
+              <div class="card-icon">
+                <i class="fas fa-flag-checkered"></i>
+              </div>
+            </div>
+            <div class="card-section">
+              <h4>Redeem for Rewards</h4>
+              <p>After you've made so many purchases, you can use your accumulated points to redeem for store rewards.</p>
+            </div>
+          </div>
+        </div>
+        <div class="cell">
+          <div class="card">
+            <div class="card-icon-circle">
+              <div class="card-icon">
+                <i class="fas fa-star"></i>
+              </div>
+            </div>
+            <div class="card-section">
+              <h4>Receive Member Benefits</h4>
+              <p>As a Fast Points member, you'll receive exclusive discounts on items purchased at our stores.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
+  </div> <!-- end how-to-earn -->
+
+  <div class="rewards-faq">
+    <div class="container">
+      <div class="inner-row">
+        <div class="section-title">
+          <h1 class="superhero">Fast Points FAQ</h1>
+        </div>
+      </div>
+
+      <div class="inner-row align-center">
+        <div class="faq-accordion">
+          <ul class="accordion" data-accordion data-allow-all-closed="true">
+            <li class="accordion-item is-active" data-accordion-item>
+              <a href="#panel1" class="accordion-title"><h6>Is there a minimum purchase amount necessary to earn points?</h6></a>
+              <div class="accordion-content" id="panel1" data-tab-content>
+                Suspendisse mauris justo, mattis at turpis ut, vestibulum ullamcorper libero.
+              </div>
+            </li>
+
+            <li class="accordion-item" data-accordion-item>
+              <a href="#panel2" class="accordion-title"><h6>How long does it take after purchase to receive points?</h6></a>
+              <div class="accordion-content" id="panel2" data-tab-content>
+                Suspendisse mauris justo, mattis at turpis ut, vestibulum ullamcorper libero.
+              </div>
+            </li>
+
+            <li class="accordion-item" data-accordion-item>
+              <a href="#panel3" class="accordion-title"><h6>Am I limited to a certain gallon amount when receiving discounts at the pump?</h6></a>
+              <div class="accordion-content" id="panel3" data-tab-content>
+                Suspendisse mauris justo, mattis at turpis ut, vestibulum ullamcorper libero.
+              </div>
+            </li>
+
+            <li class="accordion-item" data-accordion-item>
+              <a href="#panel4" class="accordion-title"><h6>Do my points expire?</h6></a>
+              <div class="accordion-content" id="panel4" data-tab-content>
+                Suspendisse mauris justo, mattis at turpis ut, vestibulum ullamcorper libero.
+              </div>
+            </li>
+
+            <li class="accordion-item" data-accordion-item>
+              <a href="#panel5" class="accordion-title"><h6>I lost my card. How do I get a new one?</h6></a>
+              <div class="accordion-content" id="panel5" data-tab-content>
+                Suspendisse mauris justo, mattis at turpis ut, vestibulum ullamcorper libero.
+              </div>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+
+    </div>
+  </div><!-- end rewards-faq -->
+
+  <div class="join-fastpoints-hero">
+    <div class="join-fastpoints-hero-content">
+      <h1 class="standard-shadow">Join Fast Points!</h1>
+      <p class="subheader">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam omnis, maxime libero natus qui minus!</p>
+      <a href="#" class="button">Join Now!</a>
+    </div>
+  </div>
+
+<?php include('footer.php'); ?>

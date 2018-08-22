532a8a3bec3d36b2c54beef333a49da4c88765f0
Alex Franco <aadf82@gmail.com>
2018-08-22 11:54:12 -0400

    Initial commit.

---
+++ foodanddrinks.php
@@ -0,0 +1,92 @@
+<?php include('header.php') ?>
+
+<div class="marketing-site-hero">
+  <div class="marketing-site-hero-content">
+    <h1 class="standard-shadow">earn rewards immediately at checkout!</h1>
+    <p class="subheader">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam omnis, maxime libero natus qui minus!</p>
+    <a href="#" class="button">learn more</a>
+  </div>
+</div>
+<section class="filter-simple">
+  <div class="tab-nav-group">
+    <button class="tab-btn filter-simple-button is-active" data-filter="food">food</button>
+    <button class="tab-btn filter-simple-button" data-filter="drinks">drinks</button>
+  </div>
+
+  <div class="stacked-wrapper inner-row food" data-equalizer data-animate="slide-in-down slide-out-up">
+    <div class="stack-media large-order-2" data-equalizer-watch>
+      <img src="img/breakfast.jpg" alt="">
+    </div>
+    <div class="stack-content container large-order-1" data-equalizer-watch>
+      <div class="stack-title">
+        <h1>Breakfast</h1>
+      </div>
+      <div class="stack-sub-title">
+        <h2>biscuits &amp; croissants</h2>
+      </div>
+      <ul class="stack-items">
+        <li>bacon, egg &amp; cheese</li>
+        <li>sausage, egg, &amp; cheese</li>
+        <li>ham, egg, &amp; cheese</li>
+      </ul>
+    </div>
+
+    <div class="stack-media large-order-3" data-equalizer-watch>
+      <img src="img/breakfast.jpg" alt="">
+    </div>
+    <div class="stack-content container large-order-4" data-equalizer-watch>
+      <div class="stack-title">
+        <h1>Breakfast</h1>
+      </div>
+      <div class="stack-sub-title">
+        <h2>biscuits &amp; croissants</h2>
+      </div>
+      <ul class="stack-items">
+        <li>bacon, egg &amp; cheese</li>
+        <li>sausage, egg, &amp; cheese</li>
+        <li>ham, egg, &amp; cheese</li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="stacked-wrapper inner-row drinks" data-equalizer data-animate="slide-in-down slide-out-up">
+    <div class="stack-media large-order-2" data-equalizer-watch>
+      <img src="img/coffee.jpg" alt="">
+    </div>
+    <div class="stack-content container large-order-1" data-equalizer-watch>
+      <div class="stack-title">
+        <h1>cold drinks</h1>
+      </div>
+      <div class="stack-sub-title">
+        <h2>sodas</h2>
+      </div>
+      <ul class="stack-items">
+        <li>coke</li>
+        <li>sprite</li>
+        <li>ginger ale</li>
+        <li>slushes</li>
+      </ul>
+    </div>
+
+    <div class="stack-media large-order-3" data-equalizer-watch>
+      <img src="img/coffee.jpg" alt="">
+    </div>
+    <div class="stack-content container large-order-4" data-equalizer-watch>
+      <div class="stack-title">
+        <h1>hot drinks</h1>
+      </div>
+      <div class="stack-sub-title">
+        <h2>coffee</h2>
+      </div>
+      <ul class="stack-items">
+        <li>house blend</li>
+        <li>dark roast</li>
+        <li>decaf</li>
+        <li>espresso</li>
+      </ul>
+    </div>
+  </div>
+
+</section>
+
+<?php include('footer.php') ?>

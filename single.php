18d9413f79fe65b8e9a3f5d7a8c673c509bef7a8
Alex Franco <aadf82@gmail.com>
2018-08-22 11:54:44 -0400

    Initial commit.

---
+++ single.php
@@ -0,0 +1,11 @@
+<?php include('header.php'); ?>
+<div class="page-hero single">
+  <div class="page-hero-content">
+    <h1 class="standard-shadow">News Post Title</h1>
+    <div class="hero-post-info">
+      <div class="date-info"></div>
+    </div>
+  </div>
+</div>
+
+<?php include('footer.php'); ?>

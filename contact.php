98a1e5270a62789463ad882e1335030a27ebd1dc
Alex Franco <aadf82@gmail.com>
2018-08-23 18:25:21 -0400

    Added contact form

--- contact.php
+++ contact.php
@@ -62,9 +62,57 @@
   </div>
 </div>
 
-.
+<div class="map-contact">
+  <div class="container">
+    <div class="inner-row">
+      <div class="map-wrapper">
+      <div class="responsive-embed">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3004.749641775663!2d-80.6749997839767!3d41.139991019256826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8833e5d438212a41%3A0xb9da7f68d5047fd9!2s1057+Trumbull+Ave%2C+Youngstown%2C+OH+44505!5e0!3m2!1sen!2sus!4v1535037782183" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </div>
+      </div>
+      <div class="contact-form-wrapper">
+        <form>
+          <div class="form-icons">
+            <h4>Contact Us</h4>
+
+            <div class="input-group">
+              <span class="input-group-label">
+                <i class="fa fa-user"></i>
+              </span>
+              <input class="input-group-field" type="text" placeholder="Full name">
+            </div>
+
+            <div class="input-group">
+              <span class="input-group-label">
+                <i class="fa fa-envelope"></i>
+              </span>
+              <input class="input-group-field" type="text" placeholder="Email">
+            </div>
+
+            <div class="input-group">
+              <span class="input-group-label">
+                <i class="fa fa-phone"></i>
+              </span>
+              <input class="input-group-field" type="text" placeholder="Phone">
+            </div>
+          </div>
+
+          <div class="input-group">
+            <span class="input-group-label">
+              <i class="fa fa-edit"></i>
+            </span>
+            <textarea class="message" placeholder="Type your message here." rows="5"></textarea>
+          </div>
+
+          <button class="button expanded">Send Message</button>
+        </form>
+      </div>
+
+    </div>
+  </div>
+</div>
 
 
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3004.749641775663!2d-80.6749997839767!3d41.139991019256826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8833e5d438212a41%3A0xb9da7f68d5047fd9!2s1057+Trumbull+Ave%2C+Youngstown%2C+OH+44505!5e0!3m2!1sen!2sus!4v1535037782183" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+
 
 <?php include('footer.php'); ?>

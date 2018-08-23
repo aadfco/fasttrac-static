77f13cff3b6bdb9a2a224e1f17b5e97062a61f59
Alex Franco <aadf82@gmail.com>
2018-08-23 13:38:40 -0400

    Initial commit

---
+++ contact.php
@@ -0,0 +1,70 @@
+<?php include('header.php'); ?>
+<div class="page-hero contact">
+  <div class="page-hero-content">
+    <h1 class="standard-shadow">contact us</h1>
+  </div>
+</div>
+
+<div class="contact-info-wrapper">
+  <div class="container">
+    <div class="inner-row align-center">
+
+      <div class="contact-info">
+        <div class="contact-circle">
+          <div class="circle-icon">
+            <i class="fas fa-phone"></i>
+          </div>
+        </div>
+        <div class="contact-details">
+          <h1>Call</h1>
+          <p>330-759-1025</p>
+        </div>
+      </div>
+
+      <div class="contact-info">
+        <div class="contact-circle">
+          <div class="circle-icon">
+            <i class="fas fa-paper-plane"></i>
+          </div>
+        </div>
+        <div class="contact-details">
+          <h1>Email</h1>
+          <p>info@fasttracstores.com</p>
+        </div>
+      </div>
+
+      <div class="contact-info">
+        <div class="contact-circle">
+          <div class="circle-icon">
+            <i class="fas fa-fax"></i>
+          </div>
+        </div>
+        <div class="contact-details">
+          <h1>fax</h1>
+          <p>330-759-1028</p>
+        </div>
+      </div>
+
+      <div class="contact-info">
+        <div class="contact-circle">
+          <div class="circle-icon">
+            <i class="fas fa-map-marker-alt"></i>
+          </div>
+        </div>
+        <div class="contact-details">
+          <h1>Address</h1>
+          <p>1057 Trumbull Ave., Suite A<span>Girard, Ohio 44420</span></p>
+
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+.
+
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3004.749641775663!2d-80.6749997839767!3d41.139991019256826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8833e5d438212a41%3A0xb9da7f68d5047fd9!2s1057+Trumbull+Ave%2C+Youngstown%2C+OH+44505!5e0!3m2!1sen!2sus!4v1535037782183" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+<?php include('footer.php'); ?>

a347bd8b506e2a602ad9508746f54a8fd0471c9c
Alex Franco <aadf82@gmail.com>
2018-08-23 18:26:34 -0400

    Logo variations

---
+++ header-2.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fast Trac Convenience Store | Gas Station Convenience Store Serving Ohio</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/app.css">
+  </head>
+  <body>
+    <div class="site-wrapper">
+      <header class="navigation">
+        <div class="top-nav">
+          <div class="container">
+            <div class="inner-row">
+              <div class="top-social">
+                <ul class="menu">
+                  <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                  <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                  <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                </ul>
+              </div>
+              <nav class="secondary-menu">
+                <ul class="menu">
+                  <li><a href="#">about us</a></li>
+                  <li><a href="./news.php">news</a></li>
+                  <li><a href="#">community</a></li>
+                  <li><a href="#">careers</a></li>
+                  <li><a href="./contact.php">contact</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div><!-- end top-nav -->
+
+        <!-- Begin Main Nav -->
+        <div class="main-nav">
+          <div class="container">
+            <div class="inner-row align-justify align-middle">
+              <div class="logo-2">
+                <a href="/fasttrac-static"><img src="img/fasttrac-logo-2.svg" alt="Fast Trac Convenience Store"></a>
+              </div>
+
+              <nav class="mobile-menu hide-for-large">
+                <div class="mobile-btn-wrap">
+                  <button class="hamburger hamburger--elastic" type="button" data-toggle="mobile-menu-overlay">
+                    <span class="hamburger-box">
+                      <span class="hamburger-inner"></span>
+                    </span>
+                  </button>
+                </div>
+                <div class="mobile-menu-overlay" id="mobile-menu-overlay" data-toggler=".mobile-menu-open" data-animate="hinge-in-from-top hinge-out-from-top">
+                  <ul class="primary-mobile-menu">
+                    <li><a href="./fastpoints.php">fast points</a></li>
+                    <li><a href="./foodanddrinks.php">fresh food</a></li>
+                    <li><a href="./foodanddrinks.php">coffee &amp; drinks</a></li>
+                    <li><a href="#">locations</a></li>
+                  </ul>
+                  <ul class="secondary-mobile-menu">
+                    <li><a href="#">about us</a></li>
+                    <li><a href="./news.php">news</a></li>
+                    <li><a href="#">community</a></li>
+                    <li><a href="#">careers</a></li>
+                    <li><a href="./contact.php">contact</a></li>
+                  </ul>
+                </div>
+              </nav>
+
+              <!-- Begin Desktop Navigation Menus -->
+              <nav class="primary-menu">
+                <ul class="menu">
+                  <li><a href="./fastpoints.php">fast points</a></li>
+                  <li><a href="./foodanddrinks.php">fresh food</a></li>
+                  <li><a href="./foodanddrinks.php">coffee &amp; drinks</a></li>
+                  <li><a href="#">locations</a></li>
+                </ul>
+              </nav>
+
+              <div class="fp-login">
+                <div class="fp-logo">
+                  <a href="#">
+                    <img src="img/fastpoints-v2.svg" alt="Fast Trac Fast Points Login | Sign up">
+                    <span class="fp-login-link">Login | Sign Up</span>
+                  </a>
+                </div>
+              </div>
+
+            </div> <!-- end inner-row -->
+          </div> <!-- end container -->
+        </div> <!-- end main-nav -->
+      </header> <!-- end navigation -->
---
+++ original-logo.php
@@ -0,0 +1,137 @@
+<?php include('header.php'); ?>
+<div class="page-hero single">
+  <div class="page-hero-content">
+    <h1 class="standard-shadow">Original Logo</h1>
+    <div class="hero-post-info">
+      <div class="date-info">August 22, 2018</div>
+      <div class="category-info">
+        <span>|</span>In <a href="#">Community</a>
+      </div>
+      <div class="author-info">
+        <span>|</span>By <a href="#">Katie Bland</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<article class="page-body">
+  <div class="post-content-wrapper">
+    <div class="container">
+      <div class="inner-row align-center">
+        <div class="post-content">
+          <div class="responsive-embed widescreen">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/djV11Xbc914?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          </div>
+          <p>Curabitur ac urna porta, imperdiet felis eget, tincidunt lacus. Phasellus accumsan nibh id felis maximus, ac sagittis diam ultrices. Donec ac magna nec sem rutrum viverra. Sed in tellus faucibus nibh euismod dictum. Nulla sem nunc, imperdiet id sem in, semper finibus leo. Nulla mattis, leo eu aliquam imperdiet, ligula neque faucibus arcu, sit amet pellentesque justo erat eget dui. In eget nulla tincidunt est pretium semper at vitae justo. Fusce nisi ipsum, interdum vel magna molestie, porta eleifend ligula. Etiam congue lobortis quam, vitae faucibus lacus laoreet eget. Etiam nec justo convallis, blandit mauris vel, gravida justo. Vivamus sed ipsum lacus.</p>
+          <p>Curabitur ac urna porta, imperdiet felis eget, tincidunt lacus. Phasellus accumsan nibh id felis maximus, ac sagittis diam ultrices. Donec ac magna nec sem rutrum viverra. Sed in tellus faucibus nibh euismod dictum. Nulla sem nunc, imperdiet id sem in, semper finibus leo. Nulla mattis, leo eu aliquam imperdiet, ligula neque faucibus arcu, sit amet pellentesque justo erat eget dui. In eget nulla tincidunt est pretium semper at vitae justo. Fusce nisi ipsum, interdum vel magna molestie, porta eleifend ligula. Etiam congue lobortis quam, vitae faucibus lacus laoreet eget. Etiam nec justo convallis, blandit mauris vel, gravida justo. Vivamus sed ipsum lacus.</p>
+          <p>Curabitur ac urna porta, imperdiet felis eget, tincidunt lacus. Phasellus accumsan nibh id felis maximus, ac sagittis diam ultrices. Donec ac magna nec sem rutrum viverra. Sed in tellus faucibus nibh euismod dictum. Nulla sem nunc, imperdiet id sem in, semper finibus leo. Nulla mattis, leo eu aliquam imperdiet, ligula neque faucibus arcu, sit amet pellentesque justo erat eget dui. In eget nulla tincidunt est pretium semper at vitae justo. Fusce nisi ipsum, interdum vel magna molestie, porta eleifend ligula. Etiam congue lobortis quam, vitae faucibus lacus laoreet eget. Etiam nec justo convallis, blandit mauris vel, gravida justo. Vivamus sed ipsum lacus.</p>
+          <blockquote>Ea possunt paria non esse. Pudebit te, inquam, illius tabulae, quam Cleanthes sane commode verbis depingere solebat. Urgent tamen et nihil remittunt. An vero displicuit ea, quae tributa est animi virtutibus tanta praestantia? Sint ista Graecorum; Cur igitur, cum de re conveniat, non malumus usitate loqui? Huius ego nunc auctoritatem sequens idem faciam.
+            <cite>Wise Man</cite>
+          </blockquote>
+          <h1>H1 Heading</h1>
+          <h2>H2 Heading</h2>
+          <h3>H3 Heading</h3>
+          <h4>H4 Heading</h4>
+          <h5>H5 Heading</h5>
+          <h6>H6 Heading</h6>
+          <p><b>Bold Text</b></p>
+          <p><strong>Strong Text</strong></p>
+          <p><i>Italicized Text</i></p>
+          <p><mark>Marked Text</marked></p>
+          <p><small>Small Text</small></p>
+          <p><del>Deleted Text</del></p>
+          <p><sub>Subscript</sub>Text</p>
+          <p><sup>Superscript</sup>Text</p>
+          <p>Curabitur ac urna porta, imperdiet felis eget, tincidunt lacus. Phasellus accumsan nibh id felis maximus, ac sagittis diam ultrices. Donec ac magna nec sem rutrum viverra. Sed in tellus faucibus nibh euismod dictum. Nulla sem nunc, imperdiet id sem in, semper finibus leo. Nulla mattis, leo eu aliquam imperdiet, ligula neque faucibus arcu, sit amet pellentesque justo erat eget dui. In eget nulla tincidunt est pretium semper at vitae justo. Fusce nisi ipsum, interdum vel magna molestie, porta eleifend ligula. Etiam congue lobortis quam, vitae faucibus lacus laoreet eget. Etiam nec justo convallis, blandit mauris vel, gravida justo. Vivamus sed ipsum lacus.</p>
+          <p>Curabitur ac urna porta, imperdiet felis eget, tincidunt lacus. Phasellus accumsan nibh id felis maximus, ac sagittis diam ultrices. Donec ac magna nec sem rutrum viverra. Sed in tellus faucibus nibh euismod dictum. Nulla sem nunc, imperdiet id sem in, semper finibus leo. Nulla mattis, leo eu aliquam imperdiet, ligula neque faucibus arcu, sit amet pellentesque justo erat eget dui. In eget nulla tincidunt est pretium semper at vitae justo. Fusce nisi ipsum, interdum vel magna molestie, porta eleifend ligula. Etiam congue lobortis quam, vitae faucibus lacus laoreet eget. Etiam nec justo convallis, blandit mauris vel, gravida justo. Vivamus sed ipsum lacus.</p>
+
+          <div class="post-gallery">
+            <div class="post-gallery-img-thumb thumbnail" data-open="img1">
+              <img src="https://placeimg.com/640/480/animals" alt="">
+              <div class="post-gallery-img-full reveal" id="img1" data-reveal>
+                <img src="https://placeimg.com/1080/920/animals" alt="">
+                <button class="close-button" data-close aria-label="Close modal" type="button">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+            </div>
+
+            <div class="post-gallery-img-thumb thumbnail" data-open="img1">
+              <img src="https://placeimg.com/640/480/animals" alt="">
+              <div class="post-gallery-img-full">
+                <img src="https://placeimg.com/1080/920/animals" alt="" class="reveal" id="img1" data-reveal>
+                <button class="close-button" data-close aria-label="Close modal" type="button">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+            </div>
+
+            <div class="post-gallery-img-thumb thumbnail" data-open="img1">
+              <img src="https://placeimg.com/640/480/animals" alt="">
+              <div class="post-gallery-img-full">
+                <img src="https://placeimg.com/1080/920/animals" alt="" class="reveal" id="img1" data-reveal>
+                <button class="close-button" data-close aria-label="Close modal" type="button">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+            </div>
+
+            <div class="post-gallery-img-thumb thumbnail" data-open="img1">
+              <img src="https://placeimg.com/640/480/animals" alt="">
+              <div class="post-gallery-img-full">
+                <img src="https://placeimg.com/1080/920/animals" alt="" class="reveal" id="img1" data-reveal>
+                <button class="close-button" data-close aria-label="Close modal" type="button">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+            </div>
+
+            <div class="post-gallery-img-thumb thumbnail" data-open="img1">
+              <img src="https://placeimg.com/640/480/animals" alt="">
+              <div class="post-gallery-img-full">
+                <img src="https://placeimg.com/1080/920/animals" alt="" class="reveal" id="img1" data-reveal>
+                <button class="close-button" data-close aria-label="Close modal" type="button">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+            </div>
+
+            <div class="post-gallery-img-thumb thumbnail" data-open="img1">
+              <img src="https://placeimg.com/640/480/animals" alt="">
+              <div class="post-gallery-img-full">
+                <img src="https://placeimg.com/1080/920/animals" alt="" class="reveal" id="img1" data-reveal>
+                <button class="close-button" data-close aria-label="Close modal" type="button">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+            </div>
+
+            <div class="post-gallery-img-thumb thumbnail" data-open="img1">
+              <img src="https://placeimg.com/640/480/animals" alt="">
+              <div class="post-gallery-img-full">
+                <img src="https://placeimg.com/1080/920/animals" alt="" class="reveal" id="img1" data-reveal>
+                <button class="close-button" data-close aria-label="Close modal" type="button">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+            </div>
+
+            <div class="post-gallery-img-thumb thumbnail" data-open="img1">
+              <img src="https://placeimg.com/640/480/animals" alt="">
+              <div class="post-gallery-img-full">
+                <img src="https://placeimg.com/1080/920/animals" alt="" class="reveal" id="img1" data-reveal>
+                <button class="close-button" data-close aria-label="Close modal" type="button">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+            </div>
+
+          </div><!-- end post-gallery -->
+
+        </div> <!-- End post-content -->
+      </div> <!-- end inner-row -->
+    </div> <!-- end container -->
+  </div> <!-- End post-content-wrapper -->
+</article>
+
+<?php include('footer.php'); ?>

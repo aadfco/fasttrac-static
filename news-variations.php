1b9bc9d2654b194a6d152c5ae22a3a109cce7638
Alex Franco <aadf82@gmail.com>
2018-08-28 15:34:06 -0400

    Initial commit.

---
+++ news-variations.php
@@ -0,0 +1,149 @@
+<?php include('header.php'); ?>
+<div class="page-hero news">
+  <div class="page-hero-content">
+    <h1 class="standard-shadow">Fast Trac News</h1>
+    <p class="subheader">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam omnis, maxime libero natus qui minus!</p>
+  </div>
+</div>
+
+<div class="news-posts">
+  <div class="container">
+    <div class="inner-row grid-margin-x small-up-1 large-up-3">
+
+      <article class="news-card">
+        <div class="card-image">
+          <img src="img/featured_post_image-thumb.jpg" alt="">
+        </div>
+        <div class="post-category">
+          <i class="fas fa-folder-open"></i>
+          <span>community</span>
+        </div>
+        <header>
+          <h1>News Post Title</h1>
+        </header>
+        <div class="post-date">
+          <i class="fas fa-calendar-alt"></i>
+          <span>August 19, 2018</span>
+        </div>
+        <div class="post-excerpt">
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever...</p>
+        </div>
+        <a href="./single.php" class="button">Read More</a>
+      </article>
+
+      <article class="news-card">
+        <div class="card-image">
+          <img src="img/featured_post_image-thumb.jpg" alt="">
+        </div>
+        <div class="post-category">
+          <i class="fas fa-folder-open"></i>
+          <span>community</span>
+        </div>
+        <header class="alternate">
+          <h1>News Post Title</h1>
+        </header>
+        <div class="post-date">
+          <i class="fas fa-calendar-alt"></i>
+          <span>August 19, 2018</span>
+        </div>
+        <div class="post-excerpt">
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever...</p>
+        </div>
+        <div class="post-button">
+          <a href="./single.php" class="button">Read More</a>
+        </div>
+      </article>
+
+      <article class="news-card-2"><a href="./single.php" class="post-link">
+        <div class="card-image">
+          <img src="img/featured_post_image-thumb.jpg" alt="">
+        </div>
+        <header>
+          <h1>News Post Title</h1>
+        </header>
+        <div class="post-date">
+          <i class="fas fa-calendar-alt"></i>
+          <span>August 19, 2018</span>
+        </div>
+        <div class="post-excerpt">
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever...</p>
+        </div>
+        <!-- <a href="#" class="button">Read More</a> -->
+        </a>
+        <div class="post-category-2">
+          <i class="fas fa-folder-open"></i>
+          <a href="#"><span>community</span></a>
+        </div>
+      </article>
+
+      <article class="news-card">
+        <div class="card-image">
+          <img src="img/featured_post_image-thumb.jpg" alt="">
+        </div>
+        <div class="post-category">
+          <i class="fas fa-folder-open"></i>
+          <span>community</span>
+        </div>
+        <header>
+          <h1>News Post Title</h1>
+        </header>
+        <div class="post-date">
+          <i class="fas fa-calendar-alt"></i>
+          <span>August 19, 2018</span>
+        </div>
+        <div class="post-excerpt">
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever...</p>
+        </div>
+        <a href="./single.php" class="button">Read More</a>
+      </article>
+
+      <article class="news-card">
+        <div class="card-image">
+          <img src="img/featured_post_image-thumb.jpg" alt="">
+        </div>
+        <div class="post-category">
+          <i class="fas fa-folder-open"></i>
+          <span>community</span>
+        </div>
+        <header class="alternate">
+          <h1>News Post Title</h1>
+        </header>
+        <div class="post-date">
+          <i class="fas fa-calendar-alt"></i>
+          <span>August 19, 2018</span>
+        </div>
+        <div class="post-excerpt">
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever...</p>
+        </div>
+        <div class="post-button">
+          <a href="./single.php" class="button">Read More</a>
+        </div>
+      </article>
+
+      <article class="news-card-2"><a href="./single.php" class="post-link">
+        <div class="card-image">
+          <img src="img/featured_post_image-thumb.jpg" alt="">
+        </div>
+        <header>
+          <h1>News Post Title</h1>
+        </header>
+        <div class="post-date">
+          <i class="fas fa-calendar-alt"></i>
+          <span>August 19, 2018</span>
+        </div>
+        <div class="post-excerpt">
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever...</p>
+        </div>
+        <!-- <a href="#" class="button">Read More</a> -->
+        </a>
+        <div class="post-category-2">
+          <i class="fas fa-folder-open"></i>
+          <a href="#"><span>community</span></a>
+        </div>
+      </article>
+
+    </div>
+  </div>
+</div>
+
+<?php include('footer.php'); ?>

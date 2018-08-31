23dcf5251db60c8436357c81c487bb56551639b1
Alex Franco <aadf82@gmail.com>
2018-08-31 10:53:59 -0400

    Moved custom scripts from functions.js to app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,48 @@
-$(document).foundation();
+$(document).foundation().ready( function(){
+  $('.hamburger').click(function(){
+		$(this).toggleClass('is-active');
+	});
+
+
+	var  mn = $(".main-nav");
+	    mms = "mobile-menu-scroll";
+	    hh = $('header').height();
+
+	$(window).scroll(function() {
+	  if( $(this).scrollTop() > 250 ) {
+	    mn.addClass(mms);
+	  } else {
+	    mn.removeClass(mms);
+	  }
+	});
+
+
+	//Hide Drinks on page load
+	$(".drinks").hide();
+
+	// shows and hides filtered items
+	$(".filter-simple-button").click(function() {
+	  var value = $(this).attr('data-filter');
+	    $(".stacked-wrapper").not('.'+value).hide();
+	    $('.stacked-wrapper').filter('.'+value).show();
+	});
+
+	// changes active class on filter buttons
+	$('.filter-simple-button').click(function () {
+	  $(this).siblings().removeClass('is-active');
+	  $(this).addClass('is-active');
+	});
+
+	// Wow Animation Settings and Initialization
+	wow = new WOW(
+		{
+		boxClass:						'wow',
+		anmiateClass:				'animated',
+		offset:							0,
+		mobile:							true,
+		live:								true
+	}
+	)
+	wow.init();
+
+});
--- js/functions.js
+++ js/functions.js
@@ -1,36 +1,3 @@
 $(document).ready(function(){
-	$('.hamburger').click(function(){
-		$(this).toggleClass('is-active');
-	});
-
-
-var  mn = $(".main-nav");
-    mms = "mobile-menu-scroll";
-    hh = $('header').height();
-
-$(window).scroll(function() {
-  if( $(this).scrollTop() > 250 ) {
-    mn.addClass(mms);
-  } else {
-    mn.removeClass(mms);
-  }
-});
-
-
-//Hide Drinks on page load
-$(".drinks").hide();
-
-// shows and hides filtered items
-$(".filter-simple-button").click(function() {
-  var value = $(this).attr('data-filter');
-    $(".stacked-wrapper").not('.'+value).hide();
-    $('.stacked-wrapper').filter('.'+value).show();
-});
-
-// changes active class on filter buttons
-$('.filter-simple-button').click(function () {
-  $(this).siblings().removeClass('is-active');
-  $(this).addClass('is-active');
-});
 
 });

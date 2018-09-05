78458141757661d66d8f4ad91dedb0d179130537
Alex Franco <aadf82@gmail.com>
2018-09-05 16:24:01 -0400

    Initial commit

---
+++ about.php
@@ -0,0 +1,111 @@
+<?php include('header.php'); ?>
+
+<div class="page-hero about">
+  <div class="page-hero-content centered">
+    <h1 class="standard-shadow">about us</h1>
+    <p class="subheader">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+  </div>
+</div>
+
+<section class="about-tabs inner-row align-center">
+  <div class="container">
+    <div class="inner-row align-center">
+      <ul class="tabs" data-tabs id="example-tabs">
+        <li class="tabs-title is-active"><a href="#who" aria-selected="true">who we are</a></li>
+        <li class="tabs-title"><a data-tabs-target="what" href="#what">what we do</a></li>
+        <li class="tabs-title"><a data-tabs-target="mission" href="#mission">our mission</a></li>
+      </ul>
+
+      <div class="tabs-content" data-tabs-content="example-tabs">
+        <div class="tabs-panel is-active animated fadeIn" id="who">
+          <div class="tab-thumbnail"></div>
+          <p>Who we are vivamus hendrerit arcu sed erat molestie vehicula. Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor. Suspendisse dictum feugiat nisl ut dapibus.</p>
+        </div>
+        <div class="tabs-panel animated fadeIn" id="what">
+          <p>What we do is suspendisse dictum feugiat nisl ut dapibus.  Vivamus hendrerit arcu sed erat molestie vehicula. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor.  Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor.</p>
+        </div>
+        <div class="tabs-panel animated fadeIn" id="mission">
+          <p>Our mission is suspendisse dictum feugiat nisl ut dapibus.  Vivamus hendrerit arcu sed erat molestie vehicula. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor.  Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="stacks">
+  <div class="stacked-wrapper inner-row" data-equalizer>
+    <div class="stack-media large-order-2 success-img wow animated fadeIn" role="img" aria-label="Success Image" data-equalizer-watch>
+      <!-- <img src="img/fasttrac-day@1000px.jpg" alt=""> -->
+    </div>
+    <div class="stack-content sc-bc container large-order-1 wow animated fadeIn" data-equalizer-watch>
+      <h1>our success story</h1>
+      <p>Vivamus sit amet maximus leo, vel tincidunt diam. Morbi in arcu diam. Maecenas mollis tellus vitae lacus laoreet, et tincidunt dui venenatis. Duis ex augue, malesuada congue erat vitae, aliquam varius dui. Ut lacinia luctus eros. Pellentesque blandit eros dapibus feugiat accumsan. Curabitur vel hendrerit ante. Praesent at finibus tortor, quis blandit lorem. Sed tincidunt, eros vel dignissim hendrerit, leo neque gravida metus, ac posuere metus arcu a erat. Duis id ante eros. Suspendisse vitae lobortis ante. Aliquam maximus enim orci, a ullamcorper quam sodales non. Donec commodo pretium nisl at finibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies sodales diam, ac sollicitudin nulla ultrices vitae.</p>
+    </div>
+
+    <div class="stack-media large-order-3 choose-img wow animated fadeIn" role="img" aria-label="Why Choose Us" data-equalizer-watch>
+      <!-- <img src="img/happypeople.jpg" alt=""> -->
+    </div>
+    <div class="stack-content sc-bc container large-order-4 wow animated fadeIn" data-equalizer-watch>
+      <h1>why choose us</h1>
+      <p>Vivamus sit amet maximus leo, vel tincidunt diam. Morbi in arcu diam. Maecenas mollis tellus vitae lacus laoreet, et tincidunt dui venenatis. Duis ex augue, malesuada congue erat vitae, aliquam varius dui. Ut lacinia luctus eros. Pellentesque blandit eros dapibus feugiat accumsan. Curabitur vel hendrerit ante. Praesent at finibus tortor, quis blandit lorem. Sed tincidunt, eros vel dignissim hendrerit, leo neque gravida metus, ac posuere metus arcu a erat. Duis id ante eros. Suspendisse vitae lobortis ante. Aliquam maximus enim orci, a ullamcorper quam sodales non. Donec commodo pretium nisl at finibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies sodales diam, ac sollicitudin nulla ultrices vitae.</p>
+    </div>
+
+  </div> <!-- end stacked-wrapper -->
+</section>
+
+<div class="about-counter-bar">
+  <div class="container">
+    <div class="inner-row counter-items-container wow animatied flipInX">
+      <div class="counter-item">
+        <span class="counter-icon">
+          <i class="fas fa-smile"></i>
+        </span>
+        <span class="counter-number" data-count="1000000">
+          0
+        </span>
+        <span class="counter-text">
+          happy customers
+        </span>
+      </div>
+
+      <div class="counter-item">
+        <span class="counter-icon">
+          <i class="fas fa-building"></i>
+        </span>
+        <span class="counter-number" data-count="31">
+          0
+        </span>
+        <span class="counter-text">
+          locations
+        </span>
+      </div>
+
+      <div class="counter-item">
+        <span class="counter-icon">
+          <i class="fas fa-trophy"></i>
+        </span>
+        <span class="counter-number" data-count="110">
+          0
+        </span>
+        <span class="counter-text">
+          local awards
+        </span>
+      </div>
+
+      <div class="counter-item">
+        <span class="counter-icon">
+          <i class="fas fa-award"></i>
+        </span>
+        <span class="counter-number" data-count="25">
+          0
+        </span>
+        <span class="counter-text">
+          years in business
+        </span>
+      </div>
+
+    </div>
+  </div>
+
+</div>
+<?php include('footer.php'); ?>

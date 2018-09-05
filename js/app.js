2748554332b065f24a7a23c2244fefbe04b28589
Alex Franco <aadf82@gmail.com>
2018-09-05 16:21:46 -0400

    Added animated counter for about.

--- js/app.js
+++ js/app.js
@@ -69,3 +69,37 @@ $(document).foundation().ready( function(){
 	wow.init();
 
 });
+
+// Animated Number Counter
+var a = 0;
+$(window).scroll(function() {
+
+  var oTop = $('.counter-items-container').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.counter-number').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+
+          duration: 3000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    a = 1;
+  }
+
+});

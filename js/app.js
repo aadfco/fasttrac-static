6eab24859aeaee413a7e71568bd57ed2c92cf55e
Alex Franco <aadf82@gmail.com>
2018-09-06 12:05:45 -0400

    Fixed console error. Moved counter script within (document).

--- js/app.js
+++ js/app.js
@@ -36,10 +36,6 @@ $(document).foundation().ready( function(){
   });
 
 
-
-
-
-
 	//Hide Drinks on page load
 	$(".drinks").hide();
 
@@ -68,38 +64,37 @@ $(document).foundation().ready( function(){
 	)
 	wow.init();
 
-});
-
-// Animated Number Counter
-var a = 0;
-$(window).scroll(function() {
-
-  var oTop = $('.counter-items-container').offset().top - window.innerHeight;
-  if (a == 0 && $(window).scrollTop() > oTop) {
-    $('.counter-number').each(function() {
-      var $this = $(this),
-        countTo = $this.attr('data-count');
-      $({
-        countNum: $this.text()
-      }).animate({
-          countNum: countTo
-        },
-
-        {
-
-          duration: 3000,
-          easing: 'swing',
-          step: function() {
-            $this.text(Math.floor(this.countNum));
+  // Animated Number Counter
+  var a = 0;
+  $(window).scroll(function() {
+    var oTop = $('.counter-items-container').offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+      $('.counter-number').each(function() {
+        var $this = $(this),
+          countTo = $this.attr('data-count');
+        $({
+          countNum: $this.text()
+        }).animate({
+            countNum: countTo
           },
-          complete: function() {
-            $this.text(this.countNum);
-            //alert('finished');
-          }
 
-        });
-    });
-    a = 1;
-  }
+          {
+
+            duration: 3000,
+            easing: 'swing',
+            step: function() {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function() {
+              $this.text(this.countNum);
+              //alert('finished');
+            }
+
+          });
+      });
+      a = 1;
+    }
+
+  });
 
 });

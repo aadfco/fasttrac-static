34fa4f6cfce1410b90d07ddaf68adf39273fba7d
Alex Franco <aadf82@gmail.com>
2018-09-11 23:07:37 -0400

    Initial commit.

---
+++ careers.php
@@ -0,0 +1,88 @@
+<?php include('header.php'); ?>
+<div class="page-hero careers">
+  <div class="page-hero-content centered">
+    <h1 class="standard-shadow">Careers</h1>
+    <p class="subheader">Join the Fast Trac Team!</p>
+  </div>
+</div>
+
+<section class="filter-tabs inner-row align-center">
+  <div class="container">
+    <div class="inner-row align-center">
+      <ul class="tabs" data-tabs id="example-tabs">
+        <li class="tabs-title is-active"><a href="#jobs" aria-selected="true">jobs</a></li>
+        <li class="tabs-title"><a data-tabs-target="benefits" href="#benefits">benefits</a></li>
+        <li class="tabs-title"><a data-tabs-target="philosophy" href="#philosophy">our philosophy</a></li>
+      </ul>
+
+      <div class="tabs-content" data-tabs-content="example-tabs">
+        <div class="tabs-panel is-active animated fadeIn" id="jobs">
+          <div class="inner-row large-up-3 small-up-2">
+            <a class="image-nav-block" href="#">
+              <div class="nav-block-overlay">
+                <div class="title-overlay">
+                  customer service
+                </div>
+                <img src="./img/happypeople.jpg" alt="">
+              </div>
+            </a>
+
+            <a class="image-nav-block" href="#">
+              <div class="nav-block-overlay">
+                <div class="title-overlay">
+                  management
+                </div>
+                <img src="./img/happypeople.jpg" alt="">
+              </div>
+            </a>
+
+            <a class="image-nav-block" href="#">
+              <div class="nav-block-overlay">
+                <div class="title-overlay">
+                  regional
+                </div>
+                <img src="./img/happypeople.jpg" alt="">
+              </div>
+            </a>
+
+            <a class="image-nav-block" href="#">
+              <div class="nav-block-overlay">
+                <div class="title-overlay">
+                  food service
+                </div>
+                <img src="./img/happypeople.jpg" alt="">
+              </div>
+            </a>
+
+            <a class="image-nav-block" href="#">
+              <div class="nav-block-overlay">
+                <div class="title-overlay">
+                  maintenance
+                </div>
+                <img src="./img/happypeople.jpg" alt="">
+              </div>
+            </a>
+
+            <a class="image-nav-block" href="#">
+              <div class="nav-block-overlay">
+                <div class="title-overlay">
+                  corporate
+                </div>
+                <img src="./img/happypeople.jpg" alt="">
+              </div>
+            </a>
+        </div>
+      </div>
+
+        <div class="tabs-panel animated fadeIn" id="benefits">
+          <p>What we do is suspendisse dictum feugiat nisl ut dapibus.  Vivamus hendrerit arcu sed erat molestie vehicula. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor.  Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor.</p>
+        </div>
+        <div class="tabs-panel animated fadeIn" id="philosophy">
+          <p>Our mission is suspendisse dictum feugiat nisl ut dapibus.  Vivamus hendrerit arcu sed erat molestie vehicula. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor.  Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php include('footer.php'); ?>

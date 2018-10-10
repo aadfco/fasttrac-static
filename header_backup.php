1c0a95a3dafa7d315ac4f9f4d478ca0311689454
Alex Franco <aadf82@gmail.com>
2018-10-10 10:14:06 -0400

    Initial commit.

---
+++ header_backup.php
@@ -0,0 +1,41 @@
+<nav class="mobile-menu hide-for-large" role="navigation">
+  <div class="mobile-btn-wrap">
+    <button class="hamburger hamburger--elastic" type="button" data-toggle="mobile-menu-overlay">
+      <span class="hamburger-box">
+        <span class="hamburger-inner"></span>
+      </span>
+    </button>
+  </div>
+  <div class="mobile-menu-overlay transition-duration-300ms" role="navigation" id="mobile-menu-overlay" data-toggler data-animate="hinge-in-from-top hinge-out-from-top">
+    <ul class="primary-mobile-menu">
+      <li><a href="./fastpoints.php">fast points</a></li>
+      <li><a href="./foodanddrinks.php">food &amp; drinks</a></li>
+      <li><a href="#">locations</a></li>
+    </ul>
+    <ul class="secondary-mobile-menu">
+      <li><a href="./about.php">about us</a></li>
+      <li><a href="./news.php">news</a></li>
+      <li><a href="#">community</a></li>
+      <li><a href="./careers.php">careers</a></li>
+      <li><a href="./contact.php">contact</a></li>
+    </ul>
+  </div>
+</nav>
+
+<!-- Begin Desktop Navigation Menus -->
+<nav class="primary-menu animated fadeInRightBig slow">
+  <ul class="menu">
+    <li><a href="./fastpoints.php">fast points</a></li>
+    <li><a href="./foodanddrinks.php">food &amp; drinks</a></li>
+    <li><a href="#">locations</a></li>
+  </ul>
+</nav>
+
+<div class="fp-login animated fadeInRightBig slow">
+  <div class="fp-logo">
+    <a href="#">
+      <img src="img/fastpoints-logo.svg" alt="Fast Trac Fast Points Login | Sign up">
+      <span class="fp-login-link">Login | Sign Up</span>
+    </a>
+  </div>
+</div>

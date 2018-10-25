e3df325d4c67f08c37b6a2b2da0e8a36810482a7
Alex Franco <aadf82@gmail.com>
2018-10-24 23:14:18 -0400

    Initial commit.

---
+++ card-flip.php
@@ -0,0 +1,50 @@
+<div class="fpc-cards-wrapper">
+  <div class="grid-x grid-padding-x small-up-1 large-up-3">
+
+    <div class="cell">
+      <div class="card flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-inner-front">
+            <h4>Front Card</h4>
+            <p>This is the front of the card. It will have some contents</p>
+          </div>
+          <div class="flip-card-inner-back">
+            <h4>Back Card</h4>
+            <p>This is the back of the card. It will have some more details.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="cell">
+      <div class="card flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-inner-front">
+            <h4>Front Card</h4>
+            <p>This is the front of the card. It will have some contents</p>
+          </div>
+          <div class="flip-card-inner-back">
+            <h4>Back Card</h4>
+            <p>This is the back of the card. It will have some more details.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="cell">
+      <div class="card flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-inner-front">
+            <h4>Front Card</h4>
+            <p>This is the front of the card. It will have some contents</p>
+          </div>
+          <div class="flip-card-inner-back">
+            <h4>Back Card</h4>
+            <p>This is the back of the card. It will have some more details.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>

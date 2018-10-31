7795d7b551ac4a6c68ee8d421f6391c08aeef16c
Alex Franco <aadf82@gmail.com>
2018-10-31 04:30:06 -0400

    Restructed inner-row

--- careers.php
+++ careers.php
@@ -14,10 +14,13 @@
         <li class="tabs-title"><a data-tabs-target="benefits" href="#benefits">benefits</a></li>
         <li class="tabs-title"><a data-tabs-target="philosophy" href="#philosophy">our philosophy</a></li>
       </ul>
+      </div>
 
       <div class="tabs-content" data-tabs-content="example-tabs">
+
         <div class="tabs-panel is-active animated fadeIn" id="jobs">
           <div class="inner-row large-up-3 small-up-2">
+
             <a class="image-nav-block" href="#">
               <div class="nav-block-overlay">
                 <div class="title-overlay">
@@ -71,8 +74,8 @@
                 <img src="./img/happypeople.jpg" alt="">
               </div>
             </a>
+          </div>
         </div>
-      </div>
 
         <div class="tabs-panel animated fadeIn" id="benefits">
           <p>What we do is suspendisse dictum feugiat nisl ut dapibus.  Vivamus hendrerit arcu sed erat molestie vehicula. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor.  Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor.</p>
@@ -80,9 +83,13 @@
         <div class="tabs-panel animated fadeIn" id="philosophy">
           <p>Our mission is suspendisse dictum feugiat nisl ut dapibus.  Vivamus hendrerit arcu sed erat molestie vehicula. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor.  Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor.</p>
         </div>
+
       </div>
-    </div>
+
+
+
   </div>
+
 </section>
 
 <?php include('footer.php'); ?>
